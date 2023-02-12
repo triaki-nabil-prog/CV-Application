2c397dc4450475b168d8453437eb605093372064
triaki-nabil-prog <nabil.triaki2@gmail.com>
2023-02-12 14:06:43 +0100

    created the footer and added social media and contact svg images

--- src/components/landingPage/Footer.js
+++ src/components/landingPage/Footer.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import github from '../../assets/img/github.svg';
+import linkedin from '../../assets/img/linkedin.svg';
+import insta from '../../assets/img/instagram.svg';
+import email from '../../assets/img/email.svg';
+import phone from '../../assets/img/phone.svg';
 
 class Footer extends React.Component {
     render() {
@@ -17,16 +22,16 @@ class Footer extends React.Component {
                     <div className="footer-contact">
                         <h3>Contact Me</h3>
                         <ul>
-                            <li>Email: triaki.nabil2@gmail.com</li>
-                            <li>Phone: 555-555-5555</li>
+                            <li><img src={email} alt="email" /> <p>triaki.nabil2@gmail.com</p></li>
+                            <li><img src={phone} alt="phone" /> <p>+213 0675 31 03 97</p></li>
                         </ul>
                     </div>
                     <div className="footer-social">
                         <h3>Follow Me</h3>
                         <ul>
-                            <li><a href="https://github.com/triaki-nabil-prog">Github</a></li>
-                            <li><a href="https://www.linkedin.com/in/triaki-nabil-87a9a9189/">Linkedin</a></li>
-                            <li><a href="https://www.instagram.com/nabil_triaki/">Instagram</a></li>
+                            <li><a href="https://github.com/triaki-nabil-prog"><img src={github} alt="github" /></a></li>
+                            <li><a href="https://www.linkedin.com/in/triaki-nabil-87a9a9189/"><img src={linkedin} alt="Linkedin" /></a></li>
+                            <li><a href="https://www.instagram.com/nabil_triaki/"><img src={insta} alt="Instagram" /></a></li>
                         </ul>
                     </div>
                 </div>
